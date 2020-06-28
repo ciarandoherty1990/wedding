41b10d8f5e791481f451f5d341c721e903c80af5
Ciaran Doherty <cdohn@allstate.com>
2020-06-28 17:17:04 +0100

    added rsvp

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "@material-ui/icons/Menu";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>,
+                <Menu/>
+            </div>
+        );
+
+    }
+}
+
+export default About;
